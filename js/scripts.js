2ea07fcf3dfbd36d8bd6eb5059379b876a35d06e
Jonathan Buchner and vichitra pool <jonathan+vichitra@>
2016-08-18 14:06:12 -0700

    added final 1st method

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,62 @@
 $(function(){
 
-var numberToNumeralOnes = function(input){
-  //Variables
-  var ones ="";
-  var tens ="";
-  var hundreds ="";
-  var thousands ="";
-
-  //Ones spot
-  var ones = input.charAt(input.length -1);
-  ones = ones.replace("1","I");
-  ones = ones.replace("2","II");
-  ones = ones.replace("3","III");
-  ones = ones.replace("4","IV");
-  ones = ones.replace("5","V");
-  ones = ones.replace("6","VI");
-  ones = ones.replace("7","VII");
-  ones = ones.replace("8","VIII");
-  ones = ones.replace("9","IX");
-  ones = ones.replace("0","");
-
-  //For Tens
-  if (input.length > 1) {
-    var tens = input.charAt(input.length -2);
-    tens = tens.replace("1","I");
-    tens = tens.replace("2","II");
-    tens = tens.replace("3","III");
-    tens = tens.replace("4","IV");
-    tens = tens.replace("5","V");
-    tens = tens.replace("6","VI");
-    tens = tens.replace("7","VII");
-    tens = tens.replace("8","VIII");
-    tens = tens.replace("9","IX");
-    tens = tens.replace("0","");
-  }
-
+  var changeToRomanNumeral = function(input){
+    //Variables
+    var ones ="";
+    var tens ="";
+    var hundreds ="";
+    var thousands ="";
+    //Ones spot
+    ones = input.charAt(input.length -1);
+    ones = ones.replace("1","I");
+    ones = ones.replace("2","II");
+    ones = ones.replace("3","III");
+    ones = ones.replace("4","IV");
+    ones = ones.replace("5","V");
+    ones = ones.replace("6","VI");
+    ones = ones.replace("7","VII");
+    ones = ones.replace("8","VIII");
+    ones = ones.replace("9","IX");
+    ones = ones.replace("0","");
+    //For Tens
+    if (input.length > 1) {
+      tens = input.charAt(input.length -2);
+      tens = tens.replace("1","X");
+      tens = tens.replace("2","XX");
+      tens = tens.replace("3","XXXI");
+      tens = tens.replace("4","XL");
+      tens = tens.replace("5","L");
+      tens = tens.replace("6","LX");
+      tens = tens.replace("7","LXX");
+      tens = tens.replace("8","LXXX");
+      tens = tens.replace("9","XC");
+      tens = tens.replace("0","");
+    }
+    //For hundreds
+    if (input.length > 2) {
+      hundreds = input.charAt(input.length -3);
+      hundreds = hundreds.replace("1","C");
+      hundreds = hundreds.replace("2","CC");
+      hundreds = hundreds.replace("3","CCC");
+      hundreds = hundreds.replace("4","CD");
+      hundreds = hundreds.replace("5","D");
+      hundreds = hundreds.replace("6","DC");
+      hundreds = hundreds.replace("7","DCC");
+      hundreds = hundreds.replace("8","DCCC");
+      hundreds = hundreds.replace("9","CM");
+      hundreds = hundreds.replace("0","");
+    }
   //For hundreds
-  if (input.length > 2) {
-    var tens = input.charAt(input.length -3);
-    tens = tens.replace("1","I");
-    tens = tens.replace("2","II");
-    tens = tens.replace("3","III");
-    tens = tens.replace("4","IV");
-    tens = tens.replace("5","V");
-    tens = tens.replace("6","VI");
-    tens = tens.replace("7","VII");
-    tens = tens.replace("8","VIII");
-    tens = tens.replace("9","IX");
-    tens = tens.replace("0","");
+    if (input.length > 3) {
+     thousands = input.charAt(input.length -4);
+     thousands = thousands.replace("1","M");
+     thousands = thousands.replace("2","MM");
+     thousands = thousands.replace("3","MMM");
+      }
+    return thousands + hundreds + tens + ones;
   }
 
-
-  return thousands + hundreds + tens + ones;
-}
-
-
-var changeToRomanNumeral = function (input){
-  input = numberToNumeralOnes(input);
-  return input;
-}
-
-
-// Front End Logic
+  // Front End Logic
   $(".roman").submit(function(event){
     $("#romanNumber").empty();
     var inputNumber = $(".inputNumber").val();
